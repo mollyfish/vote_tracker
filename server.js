f97e5c644934aa1a613813fe98bc63cbda2d8364
Molly Kent <kent.molly@gmail.com>
2015-04-16 14:44:10 -0700

    add about page, error page, secret message

--- server.js
+++ server.js
@@ -12,17 +12,23 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 // everything in the public directory will be available from the root of our directory
 
+app.get('/about', function(req, res) {//translates to the URL localhost/3000/greet
+  //req is request and res is response
+  res.status(200).sendFile(__dirname + '/public/about.html');
+  //only call ONE .send
+});
+
 app.get('/secret', function(req, res) {//translates to the URL localhost/3000/greet
   //req is request and res is response
-  res.status(200).send('<h1>You found the secret page!</h1>');
+  res.status(200).send('<h1>The truth is, I\'m a dog person.</h1>');
   //only call ONE .send
 });
 
 app.get('/*', function(req, res) {
-  res.status(404).send('<h1>ERROR 404: Could not find page</h1>');
+  res.status(404).sendFile(__dirname + '/public/404.html');
 });
 
-app.listen(process.env.PORT || 3000, function(){ //environmental variable process.env pulls the port from the command line, if it doesn't exist it will go to 3000.  in the command line we can now say "PORT=5000 node server" and the server will pick up on the change.  SWEET
+app.listen(process.env.PORT || 5000, function(){ //environmental variable process.env pulls the port from the command line, if it doesn't exist it will go to 3000.  in the command line we can now say "PORT=5000 node server" and the server will pick up on the change.  SWEET
 
 //Heroku makes you go with their port, so this (environmental variables) will be important for that 
   console.log('server is running - you rule!');

d1dfc3a81dc1ec0347aa0a0be5223621e04f2f56
Molly Kent <kent.molly@gmail.com>
2015-04-16 14:12:59 -0700

    edit secret message and error

--- server.js
+++ server.js
@@ -12,18 +12,14 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 // everything in the public directory will be available from the root of our directory
 
-app.get('/greet', function(req, res) {//translates to the URL localhost/3000/greet
+app.get('/secret', function(req, res) {//translates to the URL localhost/3000/greet
   //req is request and res is response
-  res.status(200).send('<h1>Hello Stranger!</h1>');
+  res.status(200).send('<h1>You found the secret page!</h1>');
   //only call ONE .send
 });
 
-app.get('greet/:person', function(req, res) {
-  res.send('<h1>Hello' + req.params.person + '</h1>');
-});
-
 app.get('/*', function(req, res) {
-  res.status(404).send('Could not find page');
+  res.status(404).send('<h1>ERROR 404: Could not find page</h1>');
 });
 
 app.listen(process.env.PORT || 3000, function(){ //environmental variable process.env pulls the port from the command line, if it doesn't exist it will go to 3000.  in the command line we can now say "PORT=5000 node server" and the server will pick up on the change.  SWEET

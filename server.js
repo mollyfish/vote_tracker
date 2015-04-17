911e27362c1c30e172ddeb3db76ed4fb95a115df
Molly Kent <kent.molly@gmail.com>
2015-04-17 12:42:24 -0700

    make Procfile in prep for heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express = require('express');
 var app = express();
 
 //we are using a middleware (pieces of functionality) for this
-
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 // everything in the public directory will be available from the root of our directory
 
@@ -28,7 +28,7 @@ app.get('/*', function(req, res) {
   res.status(404).sendFile(__dirname + '/public/404.html');
 });
 
-app.listen(process.env.PORT || 5000, function(){ //environmental variable process.env pulls the port from the command line, if it doesn't exist it will go to 3000.  in the command line we can now say "PORT=5000 node server" and the server will pick up on the change.  SWEET
+app.listen(app.get('port'), function(){ //environmental variable process.env pulls the port from the command line, if it doesn't exist it will go to 3000.  in the command line we can now say "PORT=5000 node server" and the server will pick up on the change.  SWEET
 
 //Heroku makes you go with their port, so this (environmental variables) will be important for that 
   console.log('server is running - you rule!');
